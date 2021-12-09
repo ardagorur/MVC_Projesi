34371c71e20e5e64f4bc4e204a3ba0bac4436f9e
ardagorur <ardagrr19@gmail.com>
2021-12-10 02:13:04 +0300

    Product

---
+++ Ilk_MVC_Projesi/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Ilk_MVC_Projesi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ilk_MVC_Projesi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly NorthwindContext _context;
+        public ProductController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Products.Include(x => x.OrderDetails).OrderBy(x => x.ProductId).ToList();
+            return View(model);
+        }
+
+        public IActionResult Detail(int? ID)
+        {
+            var Product = _context.OrderDetails.FirstOrDefault(x => x.ProductId == ID);
+            if (Product == null)
+                return RedirectToAction(nameof(Index));
+            return View(Product);
+        }
+    }
+}
