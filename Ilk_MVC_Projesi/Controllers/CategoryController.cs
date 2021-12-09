9c85793ed9cd43ea57ee9037011492c566069c74
ardagorur <93353754+ardagorur@users.noreply.github.com>
2021-12-09 19:09:19 +0300

    SO

---
+++ Ilk_MVC_Projesi/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Ilk_MVC_Projesi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ilk_MVC_Projesi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var context = new NorthwindContext();
+            var data = context.Categories.OrderBy(x => x.CategoryName).ToList();
+
+            return View(data);
+        }
+    }
+}
