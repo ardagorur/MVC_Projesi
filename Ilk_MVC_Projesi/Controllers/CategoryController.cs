be56ad2ee92bde6f624c96f2c6727020e5c5aa45
ardagorur <93353754+ardagorur@users.noreply.github.com>
2021-12-09 20:53:26 +0300

    a

--- Ilk_MVC_Projesi/Controllers/CategoryController.cs
+++ Ilk_MVC_Projesi/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Ilk_MVC_Projesi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,25 @@ namespace Ilk_MVC_Projesi.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly NorthwindContext _context;
+        public CategoryController(NorthwindContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            var context = new NorthwindContext();
-            var data = context.Categories.OrderBy(x => x.CategoryName).ToList();
+            var data = _context.Categories.Include(x=> x.Products).OrderBy(x => x.CategoryName).ToList();
 
             return View(data);
+        }
+
+        public IActionResult Detail(int? ID)
+        {
+            var Category = _context.Categories.FirstOrDefault(x => x.CategoryId == ID);
+            if (Category == null)
+                return RedirectToAction(nameof(Index));
+            return View(Category);
+            
         }
     }
 }
--- Ilk_MVC_Projesi/Startup.cs
+++ Ilk_MVC_Projesi/Startup.cs
@@ -1,6 +1,8 @@
+using Ilk_MVC_Projesi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,7 @@ namespace Ilk_MVC_Projesi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Northwind")));
             services.AddControllersWithViews();
         }
 
