bf5fd98c5a0a6c38365dcd1a0f8c587770cba12f
ardagorur <93353754+ardagorur@users.noreply.github.com>
2021-12-20 21:01:29 +0300

    product delete ajax

--- Ilk_Mvc_Projesi/Controllers/apis/ProductApiController.cs
+++ Ilk_Mvc_Projesi/Controllers/apis/ProductApiController.cs
@@ -20,6 +20,7 @@ namespace Ilk_Mvc_Projesi.Controllers.apis
         }
 
         [HttpPost]
+        [Route("~/api/productapi/delete/{id?}")] 
         public IActionResult Add(ProductViewModel model)
         {
             var product = new Product()
@@ -42,6 +43,32 @@ namespace Ilk_Mvc_Projesi.Controllers.apis
             {
                 return BadRequest($"Bir hata oluştu: {ex.Message}");
                 throw;
+            }
+        }
+        [HttpPost]
+        public IActionResult Delete(int Id = 0)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var product = _dbcontext.Products.FirstOrDefault(x => x.ProductId == Id);
+            if (product == null)
+            {
+                return BadRequest("Ürün Bulunamadı.");
+            }
+            try
+            {
+                _dbcontext.Products.Remove(product);
+                _dbcontext.SaveChanges();
+                return Ok(new
+                {
+                    Message = "Ürün silme işlemi başarılı."
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
     }
