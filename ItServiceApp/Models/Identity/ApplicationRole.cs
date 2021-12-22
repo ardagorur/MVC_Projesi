62d37f9d1c1971ba2a69d409a3b5f03f9141da93
ardagorur <93353754+ardagorur@users.noreply.github.com>
2021-12-22 20:09:18 +0300

    My Context ok

---
+++ ItServiceApp/Models/Identity/ApplicationRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Models.Identity
+{
+    public class ApplicationRole:IdentityRole
+    {
+        public ApplicationRole()
+        {
+
+        }
+        public ApplicationRole(string name, string description)
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+        public string Description { get; set; }
+    }
+}
