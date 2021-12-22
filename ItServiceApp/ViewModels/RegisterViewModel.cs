2635457b5c9bb78fa7a2f5c5f766197be8dc6911
ardagorur <93353754+ardagorur@users.noreply.github.com>
2021-12-22 20:57:36 +0300

    Reg.ViewModel eksik

---
+++ ItServiceApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="İsim alanı gereklidir")]
+        [Display(Name = "Ad")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Soyisim alanı gereklidir")]
+        [Display(Name = "Soyad")]
+        [StringLength(50)]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "E-Posta alanı gereklidir")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Şifre alanı gereklidir")]
+        [Display(Name = "Şifre")]
+        [StringLength(50)]
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
