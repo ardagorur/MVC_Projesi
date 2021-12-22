87ab36dd0f012eef0fda676fffa5f713251a2dcf
ardagrr <ardagrr19@gmail.com>
2021-12-23 00:19:53 +0300

    regviewmodel ok

--- ItServiceApp/ViewModels/RegisterViewModel.cs
+++ ItServiceApp/ViewModels/RegisterViewModel.cs
@@ -10,11 +10,11 @@ namespace ItServiceApp.ViewModels
     {
         [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir")]
         public string UserName { get; set; }
-        [Required(ErrorMessage ="İsim alanı gereklidir")]
+        [Required(ErrorMessage ="Ad alanı gereklidir")]
         [Display(Name = "Ad")]
         [StringLength(50)]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Soyisim alanı gereklidir")]
+        [Required(ErrorMessage = "Soyad alanı gereklidir")]
         [Display(Name = "Soyad")]
         [StringLength(50)]
         public string Surname { get; set; }
@@ -22,9 +22,14 @@ namespace ItServiceApp.ViewModels
         [EmailAddress]
         public string Email { get; set; }
         [Required(ErrorMessage = "Şifre alanı gereklidir")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karakter olmalıdır.")]
         [Display(Name = "Şifre")]
-        [StringLength(100,MinimumLength =6,ErrorMessage ="Şifreniz minimum 6 karakter olmalıdır.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre tekrar alanı gereklidir.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre Tekrar")]
+        [Compare(nameof(Password), ErrorMessage ="Şifreler uyuşmuyor.")]
         public string ConfirmPassword { get; set; }
     }
 }
