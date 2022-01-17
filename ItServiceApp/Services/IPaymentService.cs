28b7261c16354dc456277efcc02933558b469515
ardagorur <93353754+ardagorur@users.noreply.github.com>
2022-01-17 19:01:47 +0300

    payment1

---
+++ ItServiceApp/Services/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using ItServiceApp.Models.Payment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Services
+{
+    public interface IPaymentService
+    {
+        public InstallmentModel CheckInstalment(string binNumber, decimal Price);
+        public void Pay();
+
+    }
+}
